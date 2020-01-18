95234421111e9617a1d227b6c37acc85e383b325
Kamel CHATTI <chattikamel@gmail.com>
2020-01-18 08:44:56 +0100

    Failing 'Change for 2 coins' test

--- src/test/java/MoneyChangeTest.java
+++ src/test/java/MoneyChangeTest.java
@@ -1,18 +1,24 @@
 import io.projects.moneychange.Solution;
 import io.projects.moneychange.model.Change;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class MoneyChangeTest {
 
     @Test
-    @DisplayName("Dummy test")
-    public void test() {
+    @DisplayName("Change for 2 coins")
+    public void test2Coins() {
 
-        Change change = Solution.optimalChange(4l);
+        Change change = Solution.optimalChange(2l);
 
-        Assertions.assertNotNull(change);
+        assertNotNull(change);
+
+        System.out.println(change);
+
+        assertEquals(2, change.getCoin2() * 2 + change.getBill5() * 5 + change.getBill10() * 10);
 
     }
 
