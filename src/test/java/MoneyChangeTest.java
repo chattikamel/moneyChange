c82509e74fcd44121e66c054ab137f5cad4a716f
Kamel CHATTI <chattikamel@gmail.com>
2020-01-18 08:22:40 +0100

    structure

---
+++ src/test/java/MoneyChangeTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MoneyChangeTest {
+
+    @Test
+    @DisplayName("Dummy test")
+    public void test(){
+
+    }
+
+}
